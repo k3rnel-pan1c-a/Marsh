2e64b0b17b9fb7caa58c11fd274b74b4150fda49
Anas Badr <anasihabezmc@gmail.com>
2023-08-13 17:35:52 +0300

    Added a read and a tokenize functions

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::io;
 use std::io::Write;
 
+const PROMPT_CHAR: &str = "->";
+
+struct Command {
+    keyword: String,
+    args: Vec<String>,
+}
+
 fn main() {
     //since shells are REPL, then we'll have an infinite loop
-    loop{
-        let prompt_chat: &str = "->";
-        print!("{}", prompt_chat);
-        io::stdout().flush().unwrap();
-        let mut cmd: String = String::new();
-        io::stdin().read_line(&mut cmd).expect("Failed to read the entered command");
-        let cmd_args: Vec<&str> = cmd.split(" ").collect();
-        println!("{:?}", cmd_args)
-
+    loop {
+        print_prompt_char();
+        let cmd = parse_cmd(read_cmd());
     }
+}
 
+fn print_prompt_char() {
+    print!("{} ", PROMPT_CHAR);
+    io::stdout().flush().unwrap();
+}
+
+fn read_cmd() -> String {
+    let mut cmd: String = String::new();
+    io::stdin()
+        .read_line(&mut cmd)
+        .expect("Failed to read the entered command");
+    cmd
+}
+
+fn tokenize_cmd(cmd: String) -> Command {
+    let mut cmd_args: Vec<String> = cmd
+        .split_whitespace()
+        .map(|item| item.to_string())
+        .collect();
+    Command {
+        keyword: cmd_args.remove(0),
+        args: cmd_args,
+    }
 }
